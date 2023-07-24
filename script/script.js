81310df37b5b2be9022a1f240dd451a0cd490426
Fedor Vasilev <blazer2kforever@gmail.com>
2023-07-24 05:06:04 +0200

    feat: создать попапы профиля и добавления фото

--- script/script.js
+++ script/script.js
@@ -1,22 +1,79 @@
-const addButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const popupCloseButton = document.querySelector('.popup__close-button');
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__subheading');
+
+const editButton = document.querySelector('.profile__edit-button');
+const editPopup = document.querySelector('.popup__edit-profile');
+const popupUsername = document.querySelector('.popup__input-username');
+const popupUserstatus = document.querySelector('.popup__input-userstatus');
+
+const addButton = document.querySelector('.profile__add-button');
+const addPopup = document.querySelector('.popup__add-image');
+const popupPlace = document.querySelector('.popup__input-place');
+const popupLink = document.querySelector('.popup__input-link');
+
+const popupCloseButtons = document.querySelectorAll('.popup__close-button');
+const popupSaveButtons = document.querySelectorAll('.popup__save-button');
+
 const likeButtons = document.querySelectorAll('.gallery__like-button');
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+function renderProfileInfo() {
+  popupUsername.value = profileName.textContent;
+  popupUserstatus.value = profileStatus.textContent;
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function setProfileInfo() {
+  profileName.textContent = popupUsername.value;
+  profileStatus.textContent = popupUserstatus.value;
+}
+
+function clearImageInfo() {
+  popupPlace.value = '';
+  popupLink.value = '';
+}
+
+function openPopup(item) {
+  item.classList.add('popup_opened');
+}
+
+function closePopup(item) {
+  item.classList.remove('popup_opened');
 }
 
 function toggleLike() {
   this.classList.toggle('gallery__like-button_checked');
 }
 
-addButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', () => {
+  openPopup(editPopup);
+  renderProfileInfo();
+});
+
+addButton.addEventListener('click', () => {
+  openPopup(addPopup);
+});
+
+popupSaveButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    closePopup(button.closest('.popup'));
+
+    const currentPopupClasslist = button.closest('.popup').classList;
+    if (currentPopupClasslist.contains('popup__edit-profile')) {
+      setProfileInfo();
+    } else if (currentPopupClasslist.contains('popup__add-image')) {
+      clearImageInfo();
+    }
+  });
+});
+
+popupCloseButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    closePopup(button.closest('.popup'));
+  });
+});
+
 likeButtons.forEach((button) => {
   button.addEventListener('click', toggleLike);
 });
+
+renderProfileInfo();
